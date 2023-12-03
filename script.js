1612679041e53f11de4f561ec5b7d78bc3e242b4
khoisday <nguyenphuockhoi@gmail.com>
2023-12-03 21:38:43 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function updateTime() {
     // Set the time zone to Melbourne, Australia
-    var options = { timeZone: 'Australia/Melbourne', hour12: false };
+    var options = { timeZone: 'Australia/Melbourne', hour12: false, hour: 'numeric', minute: 'numeric' };
     var currentTime = new Date().toLocaleTimeString('en-US', options);
     
     document.getElementById("current-time").textContent = " " + currentTime;
 }
 
-// Update the time every second
-setInterval(updateTime, 1000);
+// Update the time every minute
+setInterval(updateTime, 60000);
 
 // Initial call to set the time immediately
 updateTime();
