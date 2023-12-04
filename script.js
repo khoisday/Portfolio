e057b9bd278bba6f9890c786340c08d6ab9060ba
khoisday <nguyenphuockhoi@gmail.com>
2023-12-04 16:52:52 +1100

    gradient text hey am khoi
    Adding text stroke

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function updateTime() {
     // Set the time zone to Melbourne, Australia
     var options = { timeZone: 'Australia/Melbourne', hour12: false, hour: 'numeric', minute: 'numeric' };
     var currentTime = new Date().toLocaleTimeString('en-US', options);
-    
-    document.getElementById("current-time").textContent = " " + currentTime;
+
+    // Split the time into hours and minutes
+    var [hours, minutes] = currentTime.split(":");
+
+    // Display the time and make the colon blink
+    document.getElementById("current-time").innerHTML = " " + hours + '<span class="blink">:</span>' + minutes;
 }
 
 // Update the time every minute
